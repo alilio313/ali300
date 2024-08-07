25d956eda8f1679b4f8a88ee2696ca1ec043366d
alilio313 <lioali6140@gmail.com>
2024-08-07 17:17:06 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config(object):
-    # Token Bot
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN")
-
-    API_ID = int(os.environ.get("API_ID")
-
-    API_HASH = os.environ.get("API_HASH")
